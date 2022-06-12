9a3c2932311de48e2fa123ae120984cdaa307b9a
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-06-12 22:19:20 +0300

    feat: create 2 schemas, users controller and route

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users');
+//const cardsRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(usersRoutes);
+//app.use(cardsRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+};
