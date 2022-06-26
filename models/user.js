a2af63fc77dbe8360f8b782ac3f9331f134df0e0
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-06-26 18:23:01 +0300

    feat: update user schema

--- models/user.js
+++ models/user.js
@@ -1,19 +1,44 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    required: false,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    required: false,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(url) {
+        return validator.isURL(url);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Некорректный e-mail',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
