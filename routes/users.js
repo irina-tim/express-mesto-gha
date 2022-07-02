7ff5446985a11bc0c2f9734e5edd2557d02aca23
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-07-03 00:04:19 +0300

    fix: autotests errors

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const celebrate = require('celebrate');
 
 const {
   getUsers,
@@ -11,7 +12,12 @@ const {
 
 router.get('/users', getUsers);
 router.get('/users/me', getUserInfo);
-router.get('/users/:userId', getUser);
+// router.get('/users/:userId', getUser);
+router.get('/:userId', celebrate({
+  params: celebrate.Joi.object().keys({
+    userId: celebrate.string().required(),
+  }),
+}), getUser);
 // router.post('/users', createUser);
 router.patch('/users/me', updateUserInfo);
 router.patch('/users/me/avatar', updateAvatar);
