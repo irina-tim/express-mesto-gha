a22d3641c34e39e00c4e5f09d3748118e7e70c33
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-07-03 00:11:16 +0300

    fix: autotests errors

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const celebrate = require('celebrate');
 
+const urlMatchRexExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
 const {
   getUsers,
   getUser,
-  // createUser,
   getUserInfo,
   updateUserInfo,
   updateAvatar,
@@ -12,14 +13,24 @@ const {
 
 router.get('/users', getUsers);
 router.get('/users/me', getUserInfo);
-// router.get('/users/:userId', getUser);
+
 router.get('/:userId', celebrate.celebrate({
   params: celebrate.Joi.object().keys({
     userId: celebrate.Joi.string().required(),
   }),
 }), getUser);
-// router.post('/users', createUser);
-router.patch('/users/me', updateUserInfo);
-router.patch('/users/me/avatar', updateAvatar);
+
+router.patch('/users/me', celebrate.celebrate({
+  body: celebrate.Joi.object().keys({
+    name: celebrate.Joi.string().min(2).max(30),
+    about: celebrate.Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+
+router.patch('/users/me/avatar', celebrate.celebrate({
+  body: celebrate.Joi.object().keys({
+    avatar: celebrate.Joi.string().pattern(urlMatchRexExp).required(),
+  }),
+}), updateAvatar);
 
 module.exports = router;
