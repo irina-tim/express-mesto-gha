b784877cdc09d1ce6397cf669b1175efc43600d1
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-07-02 22:24:13 +0300

    fix: autotest errors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(helmet());
+// app.use(helmet());
 
 /* app.use((req, res, next) => {
   req.user = {
