c18798cecab591679c62dd866533bf004fa0fe85
Irina Timoshenko <irene.timoshenko@yandex.ru>
2022-07-03 18:43:50 +0300

    fix: review comments

--- controllers/users.js
+++ controllers/users.js
@@ -64,7 +64,13 @@ const createUser = (req, res, next) => {
       password: hash,
     }))
     .then((user) => res.status(201).send({
-      data: user,
+      data: {
+        _id: user._id,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      },
     }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
